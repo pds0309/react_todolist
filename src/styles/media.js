befeed7720a7fef8b26d672247f661ee9e8a6e87
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-11-27 20:07:33 +0900

    feat: 기본 스타일 추가

---
+++ src/styles/media.js
@@ -0,0 +1,6 @@
+export const Media = Object.freeze({
+  mobile_sm: 'screen and (max-width: 479px)',
+  mobile_lg: 'screen and (max-width: 767px)',
+  tablet: 'screen and (max-width: 991px)',
+  desktop: 'screen and (min-width: 992px)',
+});
