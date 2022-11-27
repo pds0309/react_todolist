2ce2323272d0e1b43e785b070fa5231ce1a692c1
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-11-27 22:57:07 +0900

    feat: todopage - todo 등록 기능 추가

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,11 +1,18 @@
 import Section from '../layouts/Section';
+import TodoForm from '../components/todos/TodoForm';
 import { useState } from 'react';
 
 const Todo = () => {
   const [todos, setTodos] = useState([]);
+  console.log(todos);
+  const addTodo = ({ title, contents }) => {
+    setTodos(prev => prev.concat({ id: prev.length + 1, title, contents, status: 'progress' }));
+  };
   return (
     <>
-      <Section></Section> {/*TODO: Todo Input Section */}
+      <Section>
+        <TodoForm addTodo={addTodo} />
+      </Section>
       <Section></Section> {/*TODO: Todo-list Section */}
     </>
   );
