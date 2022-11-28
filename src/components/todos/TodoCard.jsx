aab605cfc61481191e35367624cf58ee7ad8eeb3
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-11-28 15:38:20 +0900

    feat: 카드 텍스트 스타일 변경

--- src/components/todos/TodoCard.jsx
+++ src/components/todos/TodoCard.jsx
@@ -6,9 +6,9 @@ import styled from '@emotion/styled';
 const TodoCard = ({ title, contents, done, updateTodo, deleteTodo }) => {
   return (
     <StyledCard>
-      <h3>{title}</h3>
+      <TitleHeader>{title}</TitleHeader>
       <div style={{ minHeight: '80px' }}>
-        <p>{contents}</p>
+        <ContentParagraph>{contents}</ContentParagraph>
       </div>
       <HalfButtonContainer>
         <Button onClick={updateTodo} btnType="secondary" btnColor="success">
@@ -33,6 +33,7 @@ TodoCard.propTypes = {
 const StyledCard = styled.div`
   padding: 0 20px 20px 20px;
   max-width: 300px;
+  min-width: 300px;
   border-radius: ${Border.containerRadius};
   box-shadow: 0 0 5px 1px rgba(253, 126, 64, 0.5);
 `;
@@ -45,6 +46,17 @@ const HalfButtonContainer = styled.div`
   & > button {
     font-size: 0.875rem;
   }
+`;
+
+const TitleHeader = styled.h3`
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const ContentParagraph = styled.p`
+  overflow: auto;
+  height: 80px;
 `;
 
 export default TodoCard;
